934eefea0c16bdbac87687e8fde0e5c981a76957
Praditya <pradityaaldi.s@gmail.com>
2024-08-15 19:07:53 +0700

    nggo ian

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
+import Product from './pages/Product';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
+
+        <Route path="/product/:id" element={<Product />} />
+
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import AllProducts from "../components/blocks/AllProducts"
+
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+            <AllProducts />
+        </>
     )
 }
